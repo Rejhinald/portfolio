b4a3fc26773f6a9e72849ba7134f979b5d57a267
Arwin <arwinmiclat@gmail.com>
2024-07-04 01:14:15 +0800

    FRONTEND: contact+projects: WIP

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
-export default function page() {
+export default function BackgroundBeamsDemo() {
   return (
-    <div>
-      <p className='pt 300px font-bold text-4xl text-white'>Work in Progress</p>
+    <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          Work in Progress
+        </h1>
+      </div>
+      <BackgroundBeams />
     </div>
-  )
+  );
 }
