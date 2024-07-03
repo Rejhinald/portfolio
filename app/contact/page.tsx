61f2fe146e2f51e6c66822dd9771a05ce0d749f4
Arwin <arwinmiclat@gmail.com>
2024-07-04 01:11:54 +0800

    FRONTEND: contact+projects: WIP

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      
+      <p className='pt 300px font-bold text-4xl text-white'>Work in Progress</p>
     </div>
   )
 }
